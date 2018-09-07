ac2742c4d08e36b6a5cf4df4f25d87c6ed2c314d
Diego <dieps1.0@gmail.com>
2018-09-07 08:26:36 -0300

    template banner internas

---
+++ sites/all/themes/direitofavoravel/templates/views/views-view-field--banner--block-1--field-image.tpl.php
@@ -0,0 +1,8 @@
+<?php $image = image_style_url('banner_internas', $row->field_field_image[0]['raw']['uri']); ?>
+<div class="banner-internas nid-<?php print $row->nid ?>" style="height:250px;background-position: center;background-image: url(<?php print $image ?>)">
+    <div class="container">
+        <div class="caption text-center">
+            <h1><?php print $row->node_title ?></h1>
+        </div>
+    </div>
+</div>
