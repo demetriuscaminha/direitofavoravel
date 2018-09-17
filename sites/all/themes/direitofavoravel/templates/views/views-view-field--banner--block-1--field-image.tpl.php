f83378d81218a2817f3f9767b649f1de9dfb6159
Unknown <dieps1.0@gmail.com>
2018-09-17 10:36:19 -0300

    banner interna videos

--- sites/all/themes/direitofavoravel/templates/views/views-view-field--banner--block-1--field-image.tpl.php
+++ sites/all/themes/direitofavoravel/templates/views/views-view-field--banner--block-1--field-image.tpl.php
@@ -1,9 +1,16 @@
+<?php $node = menu_get_object() ?>
+
+<?php if (isset($node) && $node->type == 'videos'): ?>
+	<?php $title = $node->title ?>
+<?php else: ?>
+	<?php global $titleDownloads ?>
+    <?php $title = (isset($titleDownloads)) ? $titleDownloads : $row->node_title ?>
+<?php endif ?>
+
 <?php $image = image_style_url('banner_internas', $row->field_field_image[0]['raw']['uri']); ?>
 <div class="banner-internas nid-<?php print $row->nid ?>" style="height:250px;background-position: center;background-image: url(<?php print $image ?>)">
     <div class="container">
         <div class="caption text-center">
-        	<?php global $titleDownloads ?>
-        	<?php $title = (isset($titleDownloads)) ? $titleDownloads : $row->node_title ?>
             <h1><?php print $title ?></h1>
         </div>
     </div>
