aa79190b5ab978687c719f61719c0d9cabc1a57c
Diego <diego@avanz.com.br>
2019-03-08 10:01:44 -0300

    refatora interna de videos

--- sites/all/themes/direitofavoravel/templates/node/node--videos.tpl.php
+++ sites/all/themes/direitofavoravel/templates/node/node--videos.tpl.php
@@ -20,28 +20,28 @@
       hide($content['comments']);
       hide($content['links']);
     ?>
-	<div class="wrap-video">
-		<div class="youtube-image-thumbnail">
-		    <?php $image = ($content['field_image']) ? $content['field_image']: $content['field_youtube'] ?>
-        <?php print render($image) ?>
-		    <div class="youtube-button-play">
-		        <i class="far fa-play-circle"></i>
-		    </div>
-		</div>
+  	<div class="wrap-video">
+  		<div class="youtube-image-thumbnail">
+  		    <?php $image = (isset($content['field_image'])) ? $content['field_image']: $content['field_youtube'] ?>
+          <?php print render($image) ?>
+  		    <div class="youtube-button-play">
+  		        <i class="far fa-play-circle"></i>
+  		    </div>
+  		</div>
 
 	    <div class="youtube-video hidden">
 	    	<iframe id="youtube-player" width="100%" height="480px" src="https://www.youtube.com/embed/<?php print $content['field_youtube']['#items'][0]['video_id'] ?>?rel=0&amp;modestbranding=1&amp;enablejsapi=1&amp;wmode=opaque&amp;controls=0&amp;autohide=1&amp;iv_load_policy=3" frameborder="0" allowfullscreen=""></iframe>
-		</div>
+		  </div>
 
-		<div class="caption video-caption">
-		    <div class="title"><?php print $node->title ?></div>
-		    
-		    <?php if (isset($node->field_name[LANGUAGE_NONE][0])): ?>
-		        <div class="lawyer"><?php print $node->field_name[LANGUAGE_NONE][0]['value'] ?></div>
-		    <?php endif ?>
-		</div>	
-	</div>
-	<?php print drupal_render($content['body']) ?>
+  		<div class="caption video-caption">
+  		    <div class="title"><?php print $node->title ?></div>
+  		    
+  		    <?php if (isset($node->field_name[LANGUAGE_NONE][0])): ?>
+  		        <div class="lawyer"><?php print $node->field_name[LANGUAGE_NONE][0]['value'] ?></div>
+  		    <?php endif ?>
+  		</div>	
+  	</div>
+  	<?php print drupal_render($content['body']) ?>
   </div>
 
   <?php print render($content['links']); ?>
