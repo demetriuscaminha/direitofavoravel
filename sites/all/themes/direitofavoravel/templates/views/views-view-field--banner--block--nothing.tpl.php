4e6f16ca7b115439d01c873de21826ab691df102
Cleverson <cleversonferreira93@gmail.com>
2019-03-11 22:15:52 -0300

    Link Banner

--- sites/all/themes/direitofavoravel/templates/views/views-view-field--banner--block--nothing.tpl.php
+++ sites/all/themes/direitofavoravel/templates/views/views-view-field--banner--block--nothing.tpl.php
@@ -7,7 +7,7 @@ $image = file_create_url($row->field_field_image[0]['raw']['uri']);
 ?>
 <?php if($is_mobile) : $image_mobile = file_create_url($row->field_field_image_mobile[0]['raw']['uri']);?>
 	<div class="banner-superbaner-mobile nid-<?php print $row->nid ?>"">
-		<a class="css-link-conteudo" href="#">
+		<a class="css-link-conteudo" href="<?php print $row->field_field_link[0]['raw']['url'] ?>">
 			<img class="img-responsive" src="<?php print $image_mobile ?>">
 		</a>
 	</div>	
@@ -15,7 +15,7 @@ $image = file_create_url($row->field_field_image[0]['raw']['uri']);
 <div class="banner-superbaner nid-<?php print $row->nid ?>" style="background-position: center;background-image: url(<?php print $image ?>)">
 	<div class="container">
 		<div class="content-info">
-			<?php if (isset($row->field_field_text_top[0])): ?>
+			<!--<?php if (isset($row->field_field_text_top[0])): ?>
 				<div class="text-top">
 					<?php print $row->field_field_text_top[0]['raw']['value'] ?>
 				</div>
@@ -32,25 +32,27 @@ $image = file_create_url($row->field_field_image[0]['raw']['uri']);
 				<div class="text-bottom">
 					<?php print $row->field_field_text_bottom[0]['raw']['value'] ?>
 				</div>
-			<?php endif ?>			
+			<?php endif ?>-->			
 
 			<div class="buttons">
 				<?php if (isset($row->field_field_link[0])): ?>
-					<a href="<?php print $row->field_field_link[0]['raw']['url'] ?>" class="btn btn-outline" target="_blank">	
-						<?php print $row->field_field_link[0]['raw']['title'] ?>
-					</a>
+					<div class="css-link-<?php print $row->field_field_posicao_link[0]['raw']['value'] ?>">
+						<a href="<?php print $row->field_field_link[0]['raw']['url'] ?>" class="btn btn-default" target="_blank">	
+							<?php print $row->field_field_link[0]['raw']['title'] ?>
+						</a>
+					</div>
 				<?php endif ?>
 
-				<?php if (isset($row->field_field_link_2[0])): ?>
+				<!--<?php if (isset($row->field_field_link_2[0])): ?>
 					<a href="<?php print $row->field_field_link[0]['raw']['url'] ?>" class="btn btn-default" target="_blank">	
 						<?php print $row->field_field_link_2[0]['raw']['title'] ?>
 					</a>
-				<?php endif ?>	
+				<?php endif ?>	-->
 			</div>		
 		</div>
 	</div>
 
-	<div class="banner-bottom">
+	<!--<div class="banner-bottom">
 		<div class="container">
 			<div class="col-md-6 col-xs-12 col-1">
 				<div class="pull-right">
@@ -83,6 +85,6 @@ $image = file_create_url($row->field_field_image[0]['raw']['uri']);
 				</div>
 			</div>
 		</div>
-	</div>
+	</div>-->
 </div>
 <?php endif; ?>
